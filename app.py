2ae0fa86993ee8bf9a84c0035031ba54d7ee7efe
Harsh00slathia <harshslathia04@gmail.com>
2026-02-19 18:21:44 +0530

    app.py

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ import pickle
 # Load trained pipeline
 import os
 
-model_path = os.path.join(os.path.dirname(__file__), "rf_pipeline.pkl")
-model = pickle.load(open(model_path, "rb"))
+model_path = os.path.join(os.getcwd(), "rf_pipeline.pkl")
+
+with open(model_path, "rb") as f:
+    model = pickle.load(f)
+
 
 st.set_page_config(page_title="Retail Profit Predictor")
 
